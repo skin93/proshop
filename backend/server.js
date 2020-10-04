e97b1e50ac7c3be17c79a6303054d49f83e602f1
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-04 21:51:03 +0200

    Remove ES Modules, add mongoose & connectDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-import express from "express"
-import dotenv from "dotenv"
-import products from "./data/products.js"
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const products = require("./data/products")
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
