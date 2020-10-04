4821a29bf549cbf72aa1f8707b4262f29a429b8d
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-04 18:30:05 +0200

    Add Router & ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
 import { Container } from "react-bootstrap"
 import TheFooter from "./components/layout/TheFooter"
 import TheHeader from "./components/layout/TheHeader"
 import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <TheHeader />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <TheFooter />
-    </>
+    </Router>
   )
 }
 
