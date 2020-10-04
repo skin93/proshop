22c4b8bcd2d454c34d2d1e4bd639e063dd1a9a0b
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-04 15:43:24 +0200

    Add react-bootstrap, header & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Container } from "react-bootstrap"
+import TheFooter from "./components/layout/TheFooter"
+import TheHeader from "./components/layout/TheHeader"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To Proshop</h1>
+      <TheHeader />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <TheFooter />
     </>
   )
 }
---
+++ frontend/src/components/layout/TheHeader.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Container, Nav, Navbar } from "react-bootstrap"
+
+const TheHeader = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i class='fas fa-shopping-cart' aria-hidden='true'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i class='fas fa-user' aria-hidden='true'></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default TheHeader
