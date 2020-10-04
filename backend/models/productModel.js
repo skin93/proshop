1c9e3b112f80e9d98535160b7779168d9d90a0e2
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-04 23:35:40 +0200

    Use ES Modules again & add express-async-handler

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose"
 
 const reviewSchema = mongoose.Schema(
   {
@@ -67,4 +67,4 @@ const productSchema = mongoose.Schema(
 
 const Product = mongoose.model("Product", productSchema)
 
-module.exports = Product
+export default Product
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const colors = require("colors")
-const connectDB = require("./config/db")
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import connectDB from "./config/db.js"
+import products from "./data/products.js"
+
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
 
@@ -14,14 +16,7 @@ app.get("/", (req, res) => {
   res.send("API js running...")
 })
 
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use("/api/products", productRoutes)
 
 const PORT = process.env.PORT || 5000
 
