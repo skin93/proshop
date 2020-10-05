d860c108494567082934878c33be9b26d62c75b7
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-05 11:41:51 +0200

    Setup redux

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db.js"
-import products from "./data/products.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 import productRoutes from "./routes/productRoutes.js"
