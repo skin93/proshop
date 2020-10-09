a7b272198d0633b6665753628e7444b8b518d196
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-09 18:05:56 +0200

    Display ordet.taxPrice & add paypal script

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react"
-import { Link } from "react-router-dom"
-import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
-import { useDispatch, useSelector } from "react-redux"
-import BaseMessage from "../components/UI/BaseMessage"
-import Loader from "../components/UI/Loader"
-import { getOrderDetails } from "../actions/orderActions"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import BaseMessage from '../components/UI/BaseMessage'
+import Loader from '../components/UI/Loader'
+import { getOrderDetails } from '../actions/orderActions'
 
 const OrderScreen = ({ match }) => {
   const orderId = match.params.id
+  const [sdkReady, setSdkReady] = useState(false)
   const dispatch = useDispatch()
 
   const orderDetails = useSelector((state) => state.orderDetails)
   const { order, loading, error } = orderDetails
+
+  const orderPay = useSelector((state) => state.orderPay)
+  const { loading: loadingPay, success: successPay } = orderPay
 
   if (!loading) {
     //   calculate prices
@@ -22,13 +27,33 @@ const OrderScreen = ({ match }) => {
     order.itemsPrice = addDecimals(
       order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
     )
+
+    order.taxPrice = addDecimals(Number(0.15 * order.itemsPrice))
   }
 
   useEffect(() => {
-    if (!order || order._id !== orderId) {
-      dispatch(getOrderDetails(orderId))
+    const addPaypalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `http://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
     }
-  }, [order, orderId])
+
+    if (!order || successPay) {
+      dispatch(getOrderDetails(orderId))
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPaypalScript()
+      } else {
+        setSdkReady(true)
+      }
+    }
+  }, [dispatch, order, orderId, successPay])
 
   return loading ? (
     <Loader />
@@ -53,7 +78,7 @@ const OrderScreen = ({ match }) => {
               <p>
                 <strong>Address: </strong>
                 {order.shippingAddress.address}, {order.shippingAddress.city},
-                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.postalCode},{' '}
                 {order.shippingAddress.country}
               </p>
               {order.isPaid ? (
