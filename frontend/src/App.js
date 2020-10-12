e92018268ada7f603e9e470d126876f2cd7bb43b
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-12 12:31:41 +0200

    Add getOrders functionality & OrderListScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import OrderListScreen from './screens/OrderListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
@@ -37,6 +38,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
         </Container>
