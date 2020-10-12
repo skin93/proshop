a66ff2b48ce7961d97534266d18ed7eb7d390ac6
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-12 11:44:34 +0200

    Config image upload & add upload endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
-import colors from "colors"
-import connectDB from "./config/db.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -16,17 +18,21 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("API js running...")
+app.get('/', (req, res) => {
+  res.send('API js running...')
 })
 
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/orders", orderRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.get("/api/config/paypal", (req, res) =>
+app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
